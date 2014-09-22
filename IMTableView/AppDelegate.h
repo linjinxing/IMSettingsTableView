6b54c6c2b05336145bc6c40b88e77e19692675de
LoveYouForever <LoveYouForever@TestdeMacBook-Pro.local>
2014-09-22 10:16:32 +0800

    创建工程

---
+++ IMTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMTableView
+//
+//  Created by LoveYouForever on 9/22/14.
+//  Copyright (c) 2014 i-Move. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IMTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IMTableView
+//
+//  Created by LoveYouForever on 9/22/14.
+//  Copyright (c) 2014 i-Move. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
